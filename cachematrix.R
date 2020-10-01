a3689f96b16b22933bd4a5a0d2a0447cd3957487
Echo523 <72233730+Echo523@users.noreply.github.com>
2020-10-01 19:55:28 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,15 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- functioinverse <- NULL
+  set <- function(y){
+    x <<- y
+    inverse <<- NULL
+  }
+  get <- function() {x}
+  setInverse <- function(inverse) {inverse <<- inverse}
+  getInverse <- function() {inv}
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)n(x = matrix()) {
 
 }
 
@@ -11,5 +19,13 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        Inverse <- x$getInverse()
+  if(!is.null(inverse)){
+    message("Getting  Chached Data")
+    return(inverse)
+  }
+  matrix <- x$get()
+  inv <- solve(matrix, ...)
+  x$setInverse(inverse)
+  inverse## Return a matrix that is the inverse of 'x'
 }
